b8d2f1d024b1e39ee951fe1bccec1e78ac24dd07
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-06-01 17:09:09 +0200

    fix footer blue

--- src/components/baseFooter.vue
+++ src/components/baseFooter.vue
@@ -1,16 +1,31 @@
 <template>
  <footer class="footer">
     <div class="footer__blue">
-       <img src="../assets/img/img/buy-comics-digital-comics.png" alt="">
+       <div class="footer__img">
+           <img src="../assets/img/img/buy-comics-digital-comics.png" alt="">
        <span>digital comics</span>
-       <img src="../assets/img/img/buy-comics-merchandise.png" alt="">
-       <span>DC merchandise</span>
-       <img src="../assets/img/img/buy-comics-subscriptions.png" alt="">
-       <span>subscrtion</span>
-       <img src="../assets/img/img/buy-comics-shop-locator.png" alt="">
-       <span>shop locator</span>
+       </div>
+       <div class="footer__img">
+          <img src="../assets/img/img/buy-comics-digital-comics.png" alt="">
+          <span>digital comics</span>
+       </div>
+       <div class="footer__img">
+           <img src="../assets/img/img/buy-comics-merchandise.png" alt="">
+           <span>DC merchandise</span>
+       </div>
+       <div class="footer__img">
+           <img src="../assets/img/img/buy-comics-subscriptions.png" alt="">
+           <span>subscrtion</span>
+       </div>
+       <div class="footer__img">
+          <img src="../assets/img/img/buy-comics-shop-locator.png" alt="">
+          <span>shop locator</span>
+       </div>
+       
        <!-- <img src="../assets/img/img/buy-dc-power-visa.svg" alt="">
        <span>DC power visa</span> -->
+    </div>
+    <div class="footer__info">
 
     </div>
  </footer> 
@@ -24,22 +39,36 @@ export default {
 
 <style lang="scss" scoped>
  .footer{
-    height: auto;
-    display: flex;
-    background-color: #0282F9;
-    justify-content: space-around;
-    align-items: center;
+    
+    
     &__blue{
-       padding: 20px;
+       display: flex;
+      padding: 20px 0;
+      display: flex;
+      background-color: #0282F9;
+      align-items: center;
+      gap: 20px;
+      justify-content: space-around;
+      
        
-    }img{
-       height: 3.125rem;
-       width: 3.125rem;
-    }
-    span{
-       margin-left:.475rem ;
+    }&__img{
+       display: flex;
+       align-items: center;
        justify-content: center;
-       color: white;
+       img{
+           height: 3.125rem;
+           width: 3.125rem;
+       }
+       span{
+            margin-left: 10px;
+            justify-content: center;
+            color: white;
+    }
+      
+    }
+    
+    &__info{
+       background-color: yellow;
     }
    
  }
