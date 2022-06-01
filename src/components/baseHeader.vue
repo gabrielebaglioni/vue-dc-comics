13bba7bf4041587dc5947de8aa7549ebb5db5c92
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-06-01 15:44:43 +0200

    inizialize header

--- src/components/baseHeader.vue
+++ src/components/baseHeader.vue
@@ -1,5 +1,22 @@
 <template>
-  <header>
+  <header class="header">
+     <div class="header__logo">
+       <img src="../assets/img/img/dc-logo.png" alt="">
+     </div>
+     <nav class="header__nav">
+       <ul class="header__list"> 
+         <li><a href="#"></a>link</li>
+         <li><a href="#"></a>link</li>
+         <li><a href="#"></a>link</li>
+         <li><a href="#"></a>link</li>
+         <li><a href="#"></a>link</li>
+         <li><a href="#"></a>link</li>
+         <li><a href="#"></a>link</li>
+         <li><a href="#"></a>link</li>
+         <li><a href="#"></a>link</li>
+         <li><a href="#"></a>link</li>
+       </ul>
+     </nav>
      
   </header>
 </template>
@@ -10,6 +27,13 @@ name: 'baseHeader',
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.header {
+    display: flex;
+    justify-content: space-around;
+    &__list{
+       display: flex;
+        list-style: none;
+    }
+}
 </style>
